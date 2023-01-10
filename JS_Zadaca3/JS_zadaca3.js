a73864d4d759ceac5f40baa2ad951ddb3dec2955
Copelinchen <maja.kovacevic567@gmail.com>
2023-01-10 15:54:16 +0100

    Zadaca JS 3

---
+++ JS_Zadaca3/JS_zadaca3.js
@@ -0,0 +1,35 @@
+var showHideCell = document.querySelectorAll('.cell');
+var onClick = document.querySelectorAll('.cell');
+
+
+function handleHide(e) {
+    var element = e.target;
+    element.classList.toggle('cell');
+}
+
+function handleShow(e) {
+    var element = e.target;
+    element.classList.toggle('cell');
+}
+
+function handleClick(e) {
+    var element = e.target;
+    element.classList.toggle('on-click');
+}
+
+
+
+for (var i = 0; i < showHideCell.length; i++) {
+    var cellHide = showHideCell[i];
+    cellHide.addEventListener('mouseover', handleHide);
+}
+
+for (var i = 0; i < showHideCell.length; i++) {
+    var cellShow = showHideCell[i];
+    cellShow.addEventListener('mouseout', handleShow);
+}
+
+for (var i = 0; i < showHideCell.length; i++) {
+    var cellClick = showHideCell[i];
+    cellClick.addEventListener('click', handleClick);
+}
